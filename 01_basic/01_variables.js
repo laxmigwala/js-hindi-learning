33725ede59764185a4f053ac245b2a1394909ba5
laxmigwala <laxmigwala857@gmail.com>
2024-05-08 17:31:59 +0000

    2nd container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 845276
+let accountEmail = "laxmigwala@gmai;.com"
+var accountPassword = "8452222"
+accountCity = "beawar"
+
+
+// accountId = "868" not allow beasure this code is use in const 
+
+accountEmail = "sajfds@gmail.com"
+accountPassword = "76456354"
+accountCity = ajmer
+
+console.log(accountId);
+console.table({accountId, accountEmail, accountPassword, accountCity})
